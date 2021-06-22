ba173f3b0056100b5145e11e8abccc050ac70110
Andrzej Precz <andrzej.precz@gmail.com>
2021-06-22 11:29:39 +0200

    add language select and new navbar

--- resources/php/content.php
+++ resources/php/content.php
@@ -4,14 +4,22 @@ namespace {
 
     class Content
     {
+        const ENCODING = 'UTF-8';
+
         const VARIABLE_NAME_SIGN = '#';
         const MODIFIER_SEPARATOR = '|';
+        const NEWLINE = "\n";
 
+        const MODIFIER_CAPITALIZE = 'capitalize';
+        const MODIFIER_CAPITALIZE_ALL = 'capitalize-all';
         const MODIFIER_ORIGINAL = 'original';
         const MODIFIER_UPPERCASE = 'uppercase';
 
-        const TITLE_VARIABLE = self::VARIABLE_NAME_SIGN . 'service-name' . self::MODIFIER_SEPARATOR . self::MODIFIER_ORIGINAL . self::VARIABLE_NAME_SIGN;
-        const ORIGINAL_LANGUAGE_VARIABLE = self::VARIABLE_NAME_SIGN . 'original-language' . self::VARIABLE_NAME_SIGN;
+        const ORIGINAL_LANGUAGE_CODE = '??';
+        const ACTIVE_LANGUAGE_CLASS = 'active';
+        const ORIGINAL_LANGUAGE_VARIABLE_NAME = 'lang-original-language';
+        const TITLE_VARIABLE = self::VARIABLE_NAME_SIGN . 'lang-service-name' . self::MODIFIER_SEPARATOR . self::MODIFIER_ORIGINAL . self::VARIABLE_NAME_SIGN;
+        const ORIGINAL_LANGUAGE_VARIABLE = self::VARIABLE_NAME_SIGN . self::ORIGINAL_LANGUAGE_VARIABLE_NAME . self::VARIABLE_NAME_SIGN;
 
         private $language;
         private $websiteTranslatedVariables;
@@ -21,6 +29,11 @@ namespace {
             $this->language = $language;
             $this->websiteTranslatedVariables = $this->getTranslatedVariables($language, $languageVariablesFilePath);
             $this->languages = $this->getTranslatedVariables($language, $languagesFilePath);
+        }
+
+        public function getLanguage(): string
+        {
+            return $this->language;
         }
 
         public function getWebsiteTranslatedVariables(): array
@@ -95,6 +108,13 @@ namespace {
                     case self::MODIFIER_UPPERCASE:
                         $value = mb_strtoupper($value);
                         break;
+                    case self::MODIFIER_CAPITALIZE:
+                        $value = mb_strtoupper(mb_substr($value, 0, 1, self::ENCODING), self::ENCODING)
+                            . mb_strtolower(mb_substr($value, 1, mb_strlen($value), self::ENCODING), self::ENCODING);
+                        break;
+                    case self::MODIFIER_CAPITALIZE_ALL:
+                        $value = mb_convert_case($value, MB_CASE_TITLE, self::ENCODING);
+                        break;
                 }
             }
 
@@ -138,9 +158,86 @@ namespace {
         public function getContent(): string
         {
             $variables = $this->getWebsiteTranslatedVariables();
-            $content = '#comming-soon# ...';
+            $content = '#lang-comming-soon# ...';
 
             return $this->getReplacedContent($content, $variables, true);
+        }
+
+        public function getSelectedLanguage(): string
+        {
+            $selectedLanguage = $this->getLanguage();
+            $languages = $this->getLanguages();
+
+            $languageName = $languages[$selectedLanguage] ?? null;
+            if ($languageName !== null) {
+                return $languageName;
+            }
+
+            $languageArr = $languages[$selectedLanguage . self::MODIFIER_SEPARATOR . self::MODIFIER_ORIGINAL] ?? null;
+            if ($languageArr !== null) {
+                return reset($languageArr);
+            }
+
+            $variables = $this->getWebsiteTranslatedVariables();
+            $content = self::VARIABLE_NAME_SIGN . self::ORIGINAL_LANGUAGE_VARIABLE_NAME . self::VARIABLE_NAME_SIGN;
+
+            return $this->getReplacedContent($content, $variables, true);
+        }
+
+        public function getSelectableLanguagesListValues(): array
+        {
+            $codes = ['' => ''];
+            $translated = ['' => self::VARIABLE_NAME_SIGN . self::ORIGINAL_LANGUAGE_VARIABLE_NAME . self::VARIABLE_NAME_SIGN];
+            $original = ['' => self::VARIABLE_NAME_SIGN . self::ORIGINAL_LANGUAGE_VARIABLE_NAME . self::MODIFIER_SEPARATOR . self::MODIFIER_ORIGINAL . self::VARIABLE_NAME_SIGN];
+
+            $allLanguages = $this->getLanguages();
+            foreach ($allLanguages as $codeOriginal => $name) {
+                $code = preg_replace('/[' . self::MODIFIER_SEPARATOR . '].*$/', '', $codeOriginal);
+                $codes[$code] = $code;
+                if (is_array($name)) {
+                    $original[$code] = reset($name);
+                } else {
+                    $translated[$code] = $name;
+                }
+            }
+
+            return [$codes, $translated, $original];
+        }
+
+        public function getSelectableLanguagesList(
+            string $protocol,
+            string $domain,
+            string $requestPath,
+            string $selectableLanguagesListHtmlFilePath
+        ): string {
+            $content = '';
+
+            $selectedLanguage = $this->getLanguage();
+            $optionContent = $this->getFileContent($selectableLanguagesListHtmlFilePath);
+            list($codesList, $translatedNamesList, $originalNamesList) = $this->getSelectableLanguagesListValues();
+
+            foreach ($codesList as $code) {
+                if ($code === '') {
+                    $name = '<b>' . mb_strtoupper(self::ORIGINAL_LANGUAGE_CODE) . '</b>: ' . $translatedNamesList[$code];
+                } else {
+                    $name = '<b>' . mb_strtoupper($code) . '</b>: ' . $originalNamesList[$code];
+
+                    if (isset($translatedNamesList[$code])
+                        && isset($originalNamesList[$code])
+                        && str_replace(self::MODIFIER_SEPARATOR . self::MODIFIER_ORIGINAL, '', $originalNamesList[$code]) !== $translatedNamesList[$code]
+                    ) {
+                        $name .= ' (' . $translatedNamesList[$code] . ')';
+                    }
+                }
+                $variables = [
+                    'href' => $protocol . ltrim($code . '.' . $domain . $requestPath, '.'),
+                    'name' => $name,
+                    'class' => $selectedLanguage === $code ? ' ' . self::ACTIVE_LANGUAGE_CLASS : '',
+                ];
+                $content .= $this->getReplacedContent($optionContent, $variables, true) . self::NEWLINE;
+            }
+
+            return $content;
         }
     }
 }
--- resources/php/index.php
+++ resources/php/index.php
@@ -13,8 +13,10 @@ const HTML_TEMPLATES_PATH = RESOURCES_PATH . 'html/';
 const PHP_PATH = RESOURCES_PATH . 'php/';
 const STYLES_PATH = RESOURCES_PATH . 'css/';
 
+$protocol = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
 $host = $_SERVER['SERVER_NAME'];
 $language = rtrim(preg_replace(DOMAIN_PATTERN, '', $host), '.');
+$domain = ltrim(mb_substr($host, mb_strlen($language)), '.');
 $requestPath = rtrim($_SERVER['REQUEST_URI'], '/');
 $lastRequestPathElement = basename($requestPath);
 
@@ -26,6 +28,7 @@ if ($lastRequestPathElement === CONTENT_MODE_SUFFIX) {
 $rootPath = preg_replace('~' . PROJECT_DIR . '.*~', PROJECT_DIR, __FILE__);
 $htmlPath = $rootPath . HTML_TEMPLATES_PATH . 'index.html';
 $bodyPath = $rootPath . HTML_TEMPLATES_PATH . "body-$suffix.html";
+$selectableLanguagesListPath = $rootPath . HTML_TEMPLATES_PATH . 'selectable-languages-list-item.html';
 $stylePath = STYLES_PATH . "style-$suffix.css";
 $languageVarsPath = $rootPath . DATA_PATH . 'website-language-variables.json';
 $languagesPath = $rootPath . DATA_PATH . 'languages.json';
@@ -35,6 +38,13 @@ $contentObj = new Content($language, $languageVarsPath, $languagesPath);
 
 $bodyVars = [
     'content' => $contentObj->getContent(),
+    'selected-language' => $contentObj->getSelectedLanguage(),
+    'selectable-languages-list' => $contentObj->getSelectableLanguagesList(
+        $protocol,
+        $domain,
+        $requestPath,
+        $selectableLanguagesListPath
+    ),
 ];
 $body = $contentObj->getFileContent($bodyPath, $bodyVars);
 
