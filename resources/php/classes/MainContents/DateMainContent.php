3d355a023f310c63e8b4f9ff6ba56c0e43afcade
Andrzej Precz <andrzej.precz@gmail.com>
2021-10-05 16:08:21 +0200

    dates view: allow to skip date for today

--- resources/php/classes/MainContents/DateMainContent.php
+++ resources/php/classes/MainContents/DateMainContent.php
@@ -17,10 +17,10 @@ class DateMainContent extends MainContent implements MainContentInterface
 
     public function configure(string $path): bool
     {
-        if (preg_match("~^/dates/(?'year'[0-9]{4})/(?'month'[0-9]{2})/(?'day'[0-9]{2})/(?'view'[-a-z0-9]+)$~", $path, $matches)) {
-            $year = (int) $matches['year'];
-            $month = (int) $matches['month'];
-            $day = (int) $matches['day'];
+        if (preg_match("~^/dates/(?'view'[-a-z0-9]+)(/(?'year'[0-9]{4})/(?'month'[0-9]{2})/(?'day'[0-9]{2}))?$~", $path, $matches)) {
+            $year = (int) ($matches['year'] ?? date('Y'));
+            $month = (int) ($matches['month'] ?? date('m'));
+            $day = (int) ($matches['day'] ?? date('d'));
             $view = $matches['view'];
 
             if (checkdate($month, $day, $year)) {
