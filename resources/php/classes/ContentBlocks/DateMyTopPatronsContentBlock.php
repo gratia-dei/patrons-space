cd4ea7d99582746989f47f51aa854bd628ea589c
Andrzej Precz <andrzej.precz@gmail.com>
2021-10-05 09:52:36 +0200

    dates page for view: my top patrons with empty data json file

--- resources/php/classes/ContentBlocks/DateMyTopPatronsContentBlock.php
+++ resources/php/classes/ContentBlocks/DateMyTopPatronsContentBlock.php
@@ -4,8 +4,26 @@ class DateMyTopPatronsContentBlock extends ContentBlock implements ContentBlockI
 {
     public function getContent(string $path, string $fileNameTranslated): string
     {
-        //...
+        $dateWithoutYear = substr($path, 5);
 
-        return $this->getOriginalHtmlFileContent('content-blocks/date-my-top-patrons-content-block.html');
+        $contentBlockContent = $this->getOriginalHtmlFileContent('content-blocks/date-my-top-patrons-content-block.html');
+        $itemContent = $this->getOriginalHtmlFileContent('items/simple-list-item.html');
+
+        $fileData = $this->getOriginalJsonFileContentArray('my-top-patrons' . self::DATA_FILE_EXTENSION);
+
+        $patronsListContent = '';
+        foreach ($fileData[$dateWithoutYear] ?? [] as $patron) {
+            $variables = [
+                'name' => $patron,
+            ];
+            $patronsListContent .= $this->getReplacedContent($itemContent, $variables);
+        }
+
+        $variables = [
+            'date' => $path,
+            'top-patrons-list' => $patronsListContent,
+        ];
+
+        return $this->getReplacedContent($contentBlockContent, $variables);
     }
 }
