f572802aa5350de4e3e814bc1dac57feaf3f2402
Andrzej Precz <andrzej.precz@gmail.com>
2022-02-11 16:37:31 +0100

    beta cards form added

--- resources/js/cards.js
+++ resources/js/cards.js
@@ -1,19 +1,56 @@
-/**
- * A4 format full size: 210mm x 297mm
- * printable area: 170mm x 247mm
- */
-const PRINT_PPI = 96;
-const CARD_WIDTH_IN_MILIMETERS = 63.5;
-const CARD_HEIGHT_IN_MILIMETERS = 88;
-const CARD_COLUMNS = 4;
-const CARD_ROWS = 2;
-const PRINTING_AREA_WIDTH_IN_MILIMETERS = CARD_WIDTH_IN_MILIMETERS * CARD_COLUMNS;
-const PRINTING_AREA_HEIGHT_IN_MILIMETERS = CARD_HEIGHT_IN_MILIMETERS * CARD_ROWS;
+const DEFAULT_PAPER_FORMAT = 'A4';
+const DEFAULT_PAPER_ORIENTATION = 'v';
+const DEFAULT_MARGIN_SIZE = 50;
+const DEFAULT_PPI = 96;
 
-const mm2px = function(milimeters) {
-  const pixels = milimeters * PRINT_PPI / 25.4;
+const ALLOWED_PAPER_FORMAT_TYPES = ['A', 'B', 'C'];
+const ALLOWED_PAPER_FORMAT_MIN_SIZE = 0;
+const ALLOWED_PAPER_FORMAT_MAX_SIZE = 6;
+const ALLOWED_PAPER_ORIENTATIONS = {
+  'v': 'vertical',
+  'h': 'horizontal'
+};
+const ALLOWED_MARGIN_MIN_SIZE = 0;
+const ALLOWED_MARGIN_MAX_SIZE = 50;
+const ALLOWED_PPI_MIN_VALUE = 30;
+const ALLOWED_PPI_MAX_VALUE = 300;
 
-  return Math.round(pixels);
+const VERTICAL_PAPER_FORMATS = {
+  "A0": {
+    "width": 841,
+    "height": 1189
+  },
+  "B0": {
+    "width": 1000,
+    "height": 1414
+  },
+  "C0": {
+    "width": 917,
+    "height": 1297
+  }
+}
+
+const CARD_WIDTH = 63.5;
+const CARD_HEIGHT = 88;
+
+const getPaperFormatSelect = function() {
+  return document.getElementById('paper-format');
+}
+
+const getPaperOrientationSelect = function() {
+  return document.getElementById('paper-orientation');
+}
+
+const getMarginSizeSelect = function() {
+  return document.getElementById('margin-size');
+}
+
+const getPpiSelect = function() {
+  return document.getElementById('ppi');
+}
+
+const getPpiTestDiv = function() {
+  return document.getElementById('ppi-test');
 }
 
 const getCanvas = function() {
@@ -26,70 +63,199 @@ const getContext = function() {
   return canvas.getContext('2d');
 }
 
-const scaleCanvas = function() {
-  var canvas = getCanvas();
+const buildForm = function() {
+  buildPaperFormatSelect();
+  buildPaperOrientationSelect();
+  buildMarginSizeSelect();
+  buildPpiSelect();
 
-  canvas.width = mm2px(PRINTING_AREA_WIDTH_IN_MILIMETERS);
-  canvas.height = mm2px(PRINTING_AREA_HEIGHT_IN_MILIMETERS);
+  buildCanvas();
+}
+
+const buildPaperFormatSelect = function() {
+  const formats = getAllowedPaperFormats();
+  const select = getPaperFormatSelect();
+
+  for (let value of formats) {
+    let option = document.createElement('option');
+
+    option.value = value;
+    option.innerHTML = value;
+    if (value === DEFAULT_PAPER_FORMAT) {
+      option.selected = 'selected';
+    }
+    select.appendChild(option);
+  }
+}
+
+const buildPaperOrientationSelect = function() {
+  const orientations = ALLOWED_PAPER_ORIENTATIONS;
+  const select = getPaperOrientationSelect();
+
+  for (let key in orientations) {
+    let value = orientations[key];
+    let option = document.createElement('option');
+
+    option.value = key;
+    option.innerHTML = value;
+    if (key === DEFAULT_PAPER_ORIENTATION) {
+      option.selected = 'selected';
+    }
+    select.appendChild(option);
+  }
+}
+
+const getAllowedPaperFormats = function() {
+  let result = [];
+
+  for (let type of ALLOWED_PAPER_FORMAT_TYPES) {
+    for (let size = ALLOWED_PAPER_FORMAT_MIN_SIZE; size <= ALLOWED_PAPER_FORMAT_MAX_SIZE; size++) {
+      result.push(type + size);
+    }
+  }
+
+  return result;
+}
+
+const buildMarginSizeSelect = function() {
+  const select = getMarginSizeSelect();
+
+  for (let value = ALLOWED_MARGIN_MIN_SIZE; value <= ALLOWED_MARGIN_MAX_SIZE; value++) {
+    let option = document.createElement('option');
+
+    option.value = value;
+    option.innerHTML = value;
+    if (value === DEFAULT_MARGIN_SIZE) {
+      option.selected = 'selected';
+    }
+    select.appendChild(option);
+  }
+}
+
+const buildPpiSelect = function() {
+  const select = getPpiSelect();
+
+  const detectedPpi = getDetectedPpi();
+  if (!Number.isInteger(detectedPpi)) {
+    detectedPpi = DEFAULT_PPI;
+  }
+
+  for (let value = ALLOWED_PPI_MIN_VALUE; value <= ALLOWED_PPI_MAX_VALUE; value++) {
+    let option = document.createElement('option');
+
+    option.value = value;
+    option.innerHTML = value;
+    if (value === detectedPpi) {
+      option.selected = 'selected';
+    }
+    select.appendChild(option);
+  }
+}
+
+const getDetectedPpi = function() {
+  const div = getPpiTestDiv();
+
+  if (div === null) {
+    return DEFAULT_PPI;
+  } else {
+    return div.offsetWidth;
+  }
+}
+
+const mm2px = function(milimeters) {
+  const ppi = getDetectedPpi();
+  const pixels = milimeters * ppi / 25.4;
+
+  return Math.round(pixels);
+}
+
+const getPrintableAreaSize = function() {
+  const format = getPaperFormatSelect().value;
+  const orientation = getPaperOrientationSelect().value;
+  const margin = getMarginSizeSelect().value;
+  const size = getFormatVerticalSize(format);
+
+  let width = Math.max(0, size.width - margin);
+  let height = Math.max(0, size.height - margin);
+
+  if (orientation !== DEFAULT_PAPER_ORIENTATION) {
+    let tmp = width;
+    width = height;
+    height = tmp;
+  }
+
+  return {
+    'width': width,
+    'height': height
+  };
+}
+
+const getFormatVerticalSize = function(format) {
+  let formatArray = format.split('');
+
+  const formatType = formatArray.shift();
+  const formatSize = formatArray.join('');
+
+  let width = VERTICAL_PAPER_FORMATS[formatType + '0'].width;
+  let height = VERTICAL_PAPER_FORMATS[formatType + '0'].height;
+
+  for (let size = 1; size <= formatSize; size++) {
+    let heightHalf = Math.floor(height / 2);
+    height = width;
+    width = heightHalf;
+  }
+
+  return {
+    'width': width,
+    'height': height
+  };
+}
+
+const buildCanvas = function() {
+  const size = getPrintableAreaSize();
+  const width = mm2px(size.width);
+  const height = mm2px(size.height);
+
+  scaleCanvas(width, height);
+  clearRectangle(0, 0, width, height);
+  drawEmptyRectangle(0, 0, width, height);
+  //...add vertical and horizontal linear scale
+  //...draw cards
+}
+
+const scaleCanvas = function(width, height) {
+  const canvas = getCanvas();
+
+  canvas.width = width;
+  canvas.height = height;
+}
+
+const clearRectangle = function(x, y, width, height) {
+  const context = getContext();
+
+  context.clearRect(0, 0, width, height);
+}
+
+const drawEmptyRectangle = function(x, y, width, height) {
+  const context = getContext();
+
+  context.lineWidth = 1;
+  context.strokeStyle = "black";
+  context.strokeRect(x, y, width, height);
 }
 
 const printCanvas = function() {
   const canvas = getCanvas();
 
-  window.document.write('<img src="' + canvas.toDataURL() + '" />');
-  window.print();
-  window.location.reload();
+  const subWindow = window.open('', '', 'width=' + screen.availWidth + ',height=' + screen.availHeight);
+  subWindow.document.open();
+  subWindow.document.write('<img src="' + canvas.toDataURL() + '">');
+  subWindow.document.addEventListener('load', function() {
+      subWindow.focus();
+      subWindow.print();
+      subWindow.document.close();
+      subWindow.close();
+  }, true);
 }
 
-const clearCanvas = function() {
-  const canvas = getCanvas();
-  const context = getContext();
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-const drawPrintableArea = function() {
-  const context = getContext();
-
-  const width = mm2px(PRINTING_AREA_WIDTH_IN_MILIMETERS);
-  const height = mm2px(PRINTING_AREA_HEIGHT_IN_MILIMETERS);
-  const x = 0;
-  const y = 0;
-
-  context.lineWidth = 1;
-  context.strokeStyle = "black";
-  context.strokeRect(x, y, width, height);
-}
-
-const drawCard = function(cardId, patronId) {
-  if (cardId < 1 || cardId > CARD_COLUMNS * CARD_ROWS || !Number.isInteger(cardId)) {
-    return;
-  }
-
-  const context = getContext();
-
-  const width = mm2px(CARD_WIDTH_IN_MILIMETERS);
-  const height = mm2px(CARD_HEIGHT_IN_MILIMETERS);
-  const x = mm2px(CARD_WIDTH_IN_MILIMETERS * ((cardId - 1) % CARD_COLUMNS));
-  const y = mm2px(CARD_HEIGHT_IN_MILIMETERS * Math.floor(cardId / CARD_COLUMNS));
-
-  context.lineWidth = 1;
-  context.strokeStyle = "black";
-  context.strokeRect(x, y, width, height);
-}
-
-const buildCanvas = function() {
-  scaleCanvas();
-  clearCanvas();
-  drawPrintableArea();
-  drawCard(1, 0);
-  drawCard(2, 0);
-  drawCard(3, 0);
-  drawCard(4, 0);
-  drawCard(5, 0);
-  drawCard(6, 0);
-  drawCard(7, 0);
-  drawCard(8, 0);
-}
-
-buildCanvas();
+buildForm();
