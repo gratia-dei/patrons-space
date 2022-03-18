3a300618c5b02576df4dc2f683fc015738de6944
Andrzej Precz <andrzej.precz@gmail.com>
2022-03-18 10:01:12 +0100

    add 2 dates list link on main page and change simple to simplified list

--- resources/php/classes/ContentBlocks/DateMyPatronsSimplifiedListContentBlock.php
+++ resources/php/classes/ContentBlocks/DateMyPatronsSimplifiedListContentBlock.php
@@ -1,12 +1,12 @@
 <?php
 
-class DateMyPatronsSimpleContentBlock extends DateMyPatronsFullContentBlock implements ContentBlockInterface
+class DateMyPatronsSimplifiedListContentBlock extends DateMyPatronsFullContentBlock implements ContentBlockInterface
 {
     protected function setProperties(): void
     {
         $immovableFilePath = $this->getGeneratedFileSuffix(self::IMMOVABLE_FILE_PATH);
         $this->immovableFileData = $this->getOriginalJsonFileContentArray($immovableFilePath);
 
-        $this->mainTemplate = $this->getOriginalHtmlFileContent('content-blocks/date-my-patrons-simple-content-block.html');
+        $this->mainTemplate = $this->getOriginalHtmlFileContent('content-blocks/date-my-patrons-simplified-list-content-block.html');
     }
 }
