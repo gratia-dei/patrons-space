6ffba0977947770fcdd6d4f848ac2411c74915a7
Andrzej Precz <andrzej.precz@gmail.com>
2022-04-22 14:44:01 +0200

    movable feasts base and move values and MovableFeastBase class skeleton

---
+++ resources/php/classes/MovableFeastBase.php
@@ -0,0 +1,44 @@
+<?php
+
+class MovableFeastBase
+{
+    public function get1956HolyFamilyFeastDate(int $year): string
+    {
+        //...
+    }
+
+    public function get1956JesusTheKingFeastDate(int $year): string
+    {
+        //...
+    }
+
+    public function get1956TheMostHolyNameOfJesusFeastDate(int $year): string
+    {
+        //...
+    }
+
+    public function get1956TheGreaterLitaniesInTheChurchOfSaintPeterDate(int $year): string
+    {
+        //...
+    }
+
+    public function get2004BaptismOfJesusFeastDate(int $year): string
+    {
+        //...
+    }
+
+    public function get2004HolyFamilyFeastDate(int $year): string
+    {
+        //...
+    }
+
+    public function getFirstSundayOfAdventDate(int $year): string
+    {
+        //...
+    }
+
+    public function getResurrectionFeastDate(int $year): string
+    {
+        //...
+    }
+}
